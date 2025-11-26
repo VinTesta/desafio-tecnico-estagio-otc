8d11d8c51290a5ebf77157b90fd5647ddc4c2e71
VinTesta <viniciustestapassos@gmail.com>
2025-11-26 16:44:44 -0300

    feat: adiciona inicialização de quote restful

---
+++ src/quote/model/quote-config.ts
@@ -0,0 +1,14 @@
+import { Token } from "@uniswap/sdk-core"
+
+export interface QuoteConfig {
+  rpc: {
+    local: string
+    mainnet: string
+  }
+  tokens: {
+    in: Token
+    amountIn: number
+    out: Token
+    poolFee: number
+  }
+}
---
+++ src/quote/quote.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { QuoteService } from './quote.service';
+import { QuoteController } from './quote.controller';
+
+@Module({
+  controllers: [QuoteController],
+  providers: [
+    QuoteService
+  ],
+})
+export class QuoteModule {}
