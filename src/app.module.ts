325ad7e26cb4167e7392d0086722dceefeb7c7d2
VinTesta <viniciustestapassos@gmail.com>
2025-11-27 02:22:45 -0300

    fix: corrige client prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuoteModule } from './quote/quote.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [QuoteModule],
+  imports: [QuoteModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
